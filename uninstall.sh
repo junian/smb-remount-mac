6e5bad3db80b374f6d1cefd272c44d1de3c98d9f
Junian Triajianto <author@junian.dev>
2024-08-13 16:43:49 +0700

    Update uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-SMB_PLIST_PATH="$HOME/Library/LaunchAgents/monitor-smb.plist"
-launchctl unload $SMB_PLIST_PATH
-rm -rf $SMB_PLIST_PATH
+LAUNCH_AGENTS_PATH="$HOME/Library/LaunchAgents/monitor-smb.plist"
 
+launchctl unload "$LAUNCH_AGENTS_PATH"
+
+if command -v trash >/dev/null 2>&1; then
+    trash "$LAUNCH_AGENTS_PATH"
+else
+    rm "$LAUNCH_AGENTS_PATH"
+fi
