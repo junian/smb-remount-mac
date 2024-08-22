c46c36498752ebc491da7c369fbd993e436db3a0
Junian Triajianto <author@junian.dev>
2024-08-22 12:52:57 +0700

    Update code

--- mount-smb.sh
+++ mount-smb.sh
@@ -1,77 +1,226 @@
 #!/bin/bash
 
-CURRENT_DIR="$(pwd)"
+# Constants
+readonly SMB_APP_VERSION=1.0.4
+readonly SMB_APP_NAME="smb-remount"
+readonly SMB_OLD_NAME="monitor-smb.plist"
 
-# Define the SMB server details, credentials, and mount options
-CONFIG_FILE="$CURRENT_DIR/.smb-remount"
+CURRENT_DIR="$(pwd)"
+readonly CURRENT_DIR
+
+readonly CONFIG_FILE="$CURRENT_DIR/.smb-remount"
+readonly LAUNCH_AGENTS_DIR="$HOME/Library/LaunchAgents"
+readonly SMB_PLIST_FILE="monitor-smb.plist"
+readonly SMB_LAUNCH_AGENT_PATH="$LAUNCH_AGENTS_DIR/$SMB_PLIST_FILE"
+
+# Variables Init
+SMB_HOST=""
+SMB_PORT=""
+SMB_PATH=""
+SMB_USERNAME=""
+SMB_PASSWORD=""
+SMB_MOUNT_POINT=""
+SMB_MOUNT_OPTIONS=""
+SMB_SERVER_MOUNT=""
+SMB_SERVER=""
+
+# GetOpts flag
+FLAG_SMB_MOUNT=0
+FLAG_SMB_UNINSTALL_LAUNCH_AGENT=0
+FLAG_SMB_DAEMON=0
+#FLAG_SMB_HELP=0
+FLAG_SMB_VERSION=0
+
+while getopts mudhv flag
+do
+    case "${flag}" in
+        m) FLAG_SMB_MOUNT=1;;
+        u) FLAG_SMB_UNINSTALL_LAUNCH_AGENT=1;;
+        d) FLAG_SMB_DAEMON=1;;
+        h) ;;
+        v) FLAG_SMB_VERSION=1;;
+        *) ;;
+    esac
+done
 
 function currentTime() {
-    date +'%H:%M:%S'
+    date +'%F %H:%M:%S'
+}
+
+function echolog() {
+    echo "[$(currentTime) LOG] $*"
+}
+
+function echoerr() {
+    echo "[$(currentTime) ERR] $*" 1>&2; 
 }
 
 function loadConfig() {
     if [ -f "$CONFIG_FILE" ]; then
         set -a
+        # shellcheck source=.smb-remount
         source "$CONFIG_FILE"
         set +a
+
+        SMB_SERVER_MOUNT="$SMB_HOST:$SMB_PORT/$SMB_PATH"
+        SMB_SERVER="//$SMB_USERNAME:$SMB_PASSWORD@$SMB_SERVER_MOUNT"
     else
-        echo "[$(currentTime) WRN] Configuration file not found: $CONFIG_FILE"
+        echoerr "Configuration file not found: $CONFIG_FILE"
     fi
 }
 
-loadConfig
+function installLaunchAgent() {
+    echolog "Installing Launch Agent ..."
 
-# Create the mount point if it doesn't exist
-if [ ! -d "$MOUNT_POINT" ]; then
-    mkdir -p "$MOUNT_POINT"
-fi
+    # Create Launch Agent dir if it doesn't exist
+    if [ ! -d "$LAUNCH_AGENTS_DIR" ]; then
+        mkdir -p "$LAUNCH_AGENTS_DIR"
+    fi
 
-# Unmount if already mounted (to remount with options)
-if mount | grep -q "$SMB_SERVER_MOUNT"; then
-    echo "[$(currentTime) INF] Unmounting $SMB_SERVER_MOUNT"
-    UNMOUNT_PATH=$(df | grep "$SMB_SERVER_MOUNT" | awk '{print $9}')
-    # umount -v "$SMB_SERVER_MOUNT"
-    diskutil unmount "$UNMOUNT_PATH"
-fi
+    export SMB_PLIST_APP_PATH="$CURRENT_DIR"
+    export SMB_PLIST_APP_NAME="$SMB_APP_NAME"
+    eval "echo \"$(cat $SMB_PLIST_FILE)\"" > "$SMB_LAUNCH_AGENT_PATH"
 
-# Mount the SMB share with the specified options
-mount -t smbfs -o "$MOUNT_OPTIONS" "$SMB_SERVER" "$MOUNT_POINT"
+    launchctl load "$SMB_LAUNCH_AGENT_PATH"
 
-# Check if the mount was successful
-if mount | grep -q "$MOUNT_POINT"; then
-    echo "[$(currentTime) INF] SMB share mounted successfully at $MOUNT_POINT with options: $MOUNT_OPTIONS"
-else
-    echo "[$(currentTime) ERR] Failed to mount SMB share"
-    exit 1
-fi
+    echolog "Finished installing Launch Agent"
+}
 
-LAUNCH_AGENTS_DIR="$HOME/Library/LaunchAgents"
+function uninstallLaunchAgent() {
+    echolog "Uninstalling existing Launch Agent ..."
 
-mkdir -p "$LAUNCH_AGENTS_DIR"
+    if [ -f "$SMB_LAUNCH_AGENT_PATH" ]
+    then
+        launchctl unload "$SMB_LAUNCH_AGENT_PATH"
+        if command -v trash >/dev/null 2>&1; then
+            trash "$SMB_LAUNCH_AGENT_PATH"
+        else
+            rm "$SMB_LAUNCH_AGENT_PATH"
+        fi
+    fi
 
-SMB_LAUNCH_AGENT_PATH="$LAUNCH_AGENTS_DIR/monitor-smb.plist"
+    echolog "Finished uninstall Launch Agent"
+}
 
-if [ -f "$SMB_LAUNCH_AGENT_PATH" ]
-then
-    launchctl unload "$SMB_LAUNCH_AGENT_PATH"
-fi
+function mountSMB() {
+    # Create the mount point if it doesn't exist
+    if [ ! -d "$SMB_MOUNT_POINT" ]; then
+        mkdir -p "$SMB_MOUNT_POINT"
+    fi
 
-export MONITOR_SMB_PATH="$CURRENT_DIR"
-eval "echo \"$(cat monitor-smb.plist)\"" > "$SMB_LAUNCH_AGENT_PATH"
+    # Unmount if already mounted (to remount with options)
+    if mount | grep -q "@$SMB_SERVER_MOUNT" | grep -q "//$SMB_USERNAME"; then
+        echolog "Unmounting \"@$SMB_SERVER_MOUNT\""
+        SMB_UNMOUNT_PATH=$(df | grep "@$SMB_SERVER_MOUNT" | grep "//$SMB_USERNAME" | awk '{print $9}')
+        diskutil unmount "$SMB_UNMOUNT_PATH"
+    fi
 
-launchctl load "$SMB_LAUNCH_AGENT_PATH"
+    # Mount the SMB share with the specified options
+    mount -t smbfs -o "$SMB_MOUNT_OPTIONS" "$SMB_SERVER" "$SMB_MOUNT_POINT"
+}
 
-# Ensure the mount point is visible in Finder Locations
-# This step uses the `open` command to open the mounted directory in Finder,
-# which should trigger Finder to add it to the Locations toolbar.
-open "$MOUNT_POINT"
+function checkMount() {
+    # Check if the mount was successful
+    if mount | grep -q "$SMB_MOUNT_POINT"; then
+        echolog "SMB share mounted successfully at $SMB_MOUNT_POINT with options: $SMB_MOUNT_OPTIONS"
+    else
+        echoerr "Failed to mount SMB share"
+        exit 1
+    fi
+}
 
-# AppleScript to add the SMB share to the Finder sidebar under Locations
-osascript <<EOF
+function openMountLocation() {
+    # Ensure the mount point is visible in Finder Locations
+    # This step uses the `open` command to open the mounted directory in Finder,
+    # which should trigger Finder to add it to the Locations toolbar.
+    open "$SMB_MOUNT_POINT"
+}
+
+function bookmarkMountLocation() {
+    # AppleScript to add the SMB share to the Finder sidebar under Locations
+    osascript <<EOF
 tell application "Finder"
     tell application "System Events" to tell process "Finder" to keystroke "t" using {command down, control down} --puts folder in sidebar, at the bottom
 end tell
 EOF
 
-# Instructions for the user to pin the mount in Locations
-echo "[$(currentTime) INF] Please pin the mount to the Locations toolbar in Finder by dragging it from the sidebar."
+    # Instructions for the user to pin the mount in Locations
+    echolog "Please pin the mount to the Locations toolbar in Finder by dragging it from the sidebar."
+}
+
+function usage() { 
+    printf "%s" "\
+Usage: smb-remount -option
+
+smb-remount is a CLI tool to remount a SMB Volume with the correct options.
+
+Options:
+
+    -m                          Mount a SMB volume based on '.smb-remount' config file and install a Launch Agent
+                                to monitor the volume.
+
+    -d                          Run a SMB monitor to check SMB Volume. If user mount it using Finder, it'll detect
+                                and remount the Volume with correct options.
+                                This usage is meant for Launch Agent.
+
+    -u                          Uninstall SMB Remount Launch Agent.
+
+    -h                          Show this usage help.
+
+    -v                          Show Version.
+
+"
+}
+
+function smbMonitorDaemon() {
+    # Monitor the mount point
+    while true; do
+        loadConfig
+
+        # Check if the mount point is mounted with the correct options
+        if mount | grep -q "$SMB_SERVER_MOUNT"; then
+            MOUNT_INFO=$(mount | grep "$SMB_SERVER_MOUNT")
+            if [[ "$MOUNT_INFO" != *"$SMB_MOUNT_POINT"* ]]; then
+                echolog "Mount options missing, remounting..."
+                mountSMB
+            fi
+        fi
+        sleep 1  # Check every 1 second
+    done
+}
+
+function main() {
+    loadConfig
+
+    if ((FLAG_SMB_MOUNT > 0)); then
+        echolog "FLAG_SMB_MOUNT"
+        mountSMB
+        checkMount
+        uninstallLaunchAgent
+        installLaunchAgent
+        openMountLocation
+        bookmarkMountLocation
+        exit 0;
+    fi
+
+    if ((FLAG_SMB_UNINSTALL_LAUNCH_AGENT > 0)); then
+        echolog "FLAG_SMB_UNINSTALL_LAUNCH_AGENT"
+        uninstallLaunchAgent
+        exit 0;
+    fi
+
+    if ((FLAG_SMB_DAEMON > 0)); then
+        echolog "FLAG_SMB_DAEMON"
+        
+        exit 0;
+    fi
+
+    if ((FLAG_SMB_VERSION > 0)); then
+        echo "$SMB_APP_NAME v$SMB_APP_VERSION"
+        exit 0;
+    fi
+
+    usage
+}
+
+main
--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-SMB_LAUNCH_AGENT_PATH="$HOME/Library/LaunchAgents/monitor-smb.plist"
-
-if [ -f "$SMB_LAUNCH_AGENT_PATH" ]
-then
-    launchctl unload "$SMB_LAUNCH_AGENT_PATH"
-    if command -v trash >/dev/null 2>&1; then
-        trash "$SMB_LAUNCH_AGENT_PATH"
-    else
-        rm "$SMB_LAUNCH_AGENT_PATH"
-    fi
-fi
-
-echo "Uninstall finished"
+./mount-smb.sh -u
